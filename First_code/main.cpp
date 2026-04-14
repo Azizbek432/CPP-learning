7ac3ff31097c8113f6e0e60c4c472fb9e82cac75
Azizbek Abdullayev <azizbekabdullayev3400@gmail.com>
2026-04-14 15:02:56 +0500

    Papka strukturasi tuzatildi va submodule o'chirildi

---
+++ First_code/main.cpp
@@ -0,0 +1,21 @@
+// Men bugun 13.04.2026y 22:36 gemini orqali o'rganib birinchi C++ kodimni yozdim!..Zo'r..
+// I wrote my first C++ code today, 13.04.2026 at 22:36, learning through gemini!..Excellent..lccls
+
+
+#include <iostream>
+#include <string>
+
+int main() {
+    // O'zgaruvchilarni e'lon qilish
+    int yosh = 14; 
+    double ball = 95.5;
+    std::string ism = "Azizbek";
+    bool dasturchimi = true;
+
+    // Konsolga chiqarish
+    std::cout << "Foydalanuvchi: " << ism << std::endl;
+    std::cout << "Yoshi: " << yosh << std::endl;
+    std::cout << "O'rtacha ball: " << ball << std::endl;
+
+    return 0;
+}
